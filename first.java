d956a72d7a2836dd9a65f343dbfc549b1a726ae6
Ekansh-Agarwal <77278829+Ekansh-Agarwal@users.noreply.github.com>
2022-05-30 21:01:31 +0530

    lab-1 question 2 added

--- first.java
+++ first.java
@@ -4,6 +4,7 @@ class first
     public static void main(String args[])throws IOException
     {
         InputStreamReader ir= new InputStreamReader(System.in);
+        //this is hello world program
         BufferedReader br= new BufferedReader(ir);
         System.out.println("Hello World");
     }
